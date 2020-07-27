6c44855dc8db4337d54f1b8d8d5c12a6ed803a07
duytvt2410 <48060188+duytvt2410@users.noreply.github.com>
2020-07-28 03:07:18 +0700

    tạo project

--- src/main/java/connection/ConnectionDB.java
+++ src/main/java/connection/ConnectionDB.java
@@ -7,9 +7,10 @@ public class ConnectionDB {
 
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		if (con == null || con.isClosed()) {
+			Class.forName("org.postgresql.Driver");
 			con = DriverManager.getConnection(
-					"jdbc:postgresql://ec2-54-197-254-117.compute-1.amazonaws.com:5432/d4v6ds4kd744at?useUnicode=true&characterEncoding=utf-8",
-					"rsdwrjxshstfoe", "7480e7a404fc17bed9b523fdb430a270024beb1b6eb59cf2df4156db2b959f81");
+					"jdbc:postgresql://ec2-54-197-254-117.compute-1.amazonaws.com:5432/d4unc42o67k0l1?useUnicode=true&characterEncoding=utf-8",
+					"bihvhwzpgntlli", "977c835dce8c1432fbde81a0d9eeb3147992ef6fa379968eece30042c65ee08c");
 			return con;
 		} else {
 			return con;
@@ -18,9 +19,10 @@ public class ConnectionDB {
 
 	public static PreparedStatement getPreparedStatement(String sql) throws ClassNotFoundException, SQLException {
 		if (con == null || con.isClosed()) {
+			Class.forName("org.postgresql.Driver");
 			con = DriverManager.getConnection(
-					"jdbc:postgresql://ec2-54-197-254-117.compute-1.amazonaws.com:5432/d4v6ds4kd744at?useUnicode=true&characterEncoding=utf-8",
-					"rsdwrjxshstfoe", "7480e7a404fc17bed9b523fdb430a270024beb1b6eb59cf2df4156db2b959f81");
+					"jdbc:postgresql://ec2-54-197-254-117.compute-1.amazonaws.com:5432/d4unc42o67k0l1?useUnicode=true&characterEncoding=utf-8",
+					"bihvhwzpgntlli", "977c835dce8c1432fbde81a0d9eeb3147992ef6fa379968eece30042c65ee08c");
 
 		}
 		return con.prepareStatement(sql);
--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class Utils {
-    static final String HOST = "https://cnpm2020.herokuapp.com//";
+    static final String HOST = "https://cnpm2020.herokuapp.com/";
 
     public static String fullPath(String path) {
         return HOST + path;
