349a5e995f51d4aeafc3178b206ef0fa6e15d478
duytvt2410 <48060188+duytvt2410@users.noreply.github.com>
2020-07-28 09:42:20 +0700

    update

--- src/main/java/connection/ConnectionDB.java
+++ src/main/java/connection/ConnectionDB.java
@@ -4,53 +4,26 @@ import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionDB {
 	static Connection con;
 
-	public static Connection getConnection() throws ClassNotFoundException, SQLException, URISyntaxException {
-
-		String dbUrl = System.getenv("JDBC_DATABASE_URL");
-
-		
-
-		if (con == null || con.isClosed()) {
-			Class.forName("org.postgresql.Driver");
-			con = DriverManager.getConnection(dbUrl);
-			return con;
-		} else {
-			return con;
-		}
+	public static Connection getConnection() throws URISyntaxException, SQLException, ClassNotFoundException {
+		Class.forName("org.postgresql.Driver");
+		String urlDB = System.getenv("JDBC_DATABASE_URL");
+		return DriverManager.getConnection(urlDB);
 
 	}
 
 	public static PreparedStatement getPreparedStatement(String sql)
 			throws ClassNotFoundException, SQLException, URISyntaxException {
-		String dbUrl = System.getenv("JDBC_DATABASE_URL");
-		if (con == null || con.isClosed()) {
-			Class.forName("org.postgresql.Driver");
-			con = DriverManager.getConnection(dbUrl);
-			return con.prepareStatement(sql);
-		} else {
-			return con.prepareStatement(sql);
-		}
-
+		Class.forName("org.postgresql.Driver");
+		String urlDB = System.getenv("JDBC_DATABASE_URL");
+		return DriverManager.getConnection(urlDB).prepareStatement(sql);
 
 	}
 
 	public static void main(String[] args) throws Exception {
-		Connection conn = getConnection();
-		PreparedStatement pst = conn.prepareStatement("select * from sanpham");
-//    	PreparedStatement pst = getPreparedStatement("select * from sanpham");
-		ResultSet rs = pst.executeQuery();
-//        int i=rs.
-		rs.last();
-		System.out.println(rs.getRow());
-		rs.beforeFirst();
-		while (rs.next()) {
-			System.out.println(rs.getString(2));
-		}
 	}
 }
